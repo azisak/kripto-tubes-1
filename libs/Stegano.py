f6c12d3f1804ce7d241d994e909feaf54c477e6a
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2019-02-13 11:31:11 +0700

    non-hardcoded version of string hiding

--- libs/Stegano.py
+++ libs/Stegano.py
@@ -1,63 +1,117 @@
 from PIL import Image
+from os.path import basename
+import binascii
 
 class SteganoBitmap:
-    def __init__(self, key, israndom):
-        self.cipher_key = key.lower()
-        if (israndom):
-            self.israndom = True
-            self.seed = self.calculate_seed(key.lower())
+    def __init__(self, signature='stg'):
+        self.signature_bin = self.string_to_bin(signature)
+
+    def get_capacity(self, img_path):
+        img = Image.open(img_path)
+        pixel_array = self.get_pixel_array(img)
+        img.close()
+
+        capacity = len(pixel_array) * len(pixel_array[0])
+        
+        return capacity
 
     def hide(self, img_path, message):
         img = Image.open(img_path)
         pixel_array = self.get_pixel_array(img)
 
-        j = 0
-    
-        print('ori : {0}'.format(pixel_array[j]))
-        
-        message_byte = message.encode("utf-8")
-        
-        print('this is byte {0}'.format(message_byte))
-        message_hex = message_byte.hex()
-        print('this is hex {0}'.format(message_hex))
-        message_bin = bin(int(message_hex, 16))[2:].zfill(8 * (len(message_hex)//2))
-        print('msg : {0} and length : {1}'.format(message_bin, len(message_bin)))
-        hiding_method = '01'
-        message_to_hide = hiding_method + message_bin
+        message_to_hide = self.build_string_to_hide(True, message, '00')
 
-        print('msg : {0} and length : {1}'.format(message_to_hide, len(message_to_hide)))
+        print('string built : {0}'.format(self.build_string_to_hide(True, message, '00')))
 
         for i in range(0, len(pixel_array)):
             pixel_array[i] = list(pixel_array[i])
 
         for i in range(0, len(message_to_hide)):
             channel = pixel_array[i//3][i%3]
-            pixel_array[i//3][i%3] = self.modify_lsb(channel, message_to_hide[i])    
+            pixel_array[i//3][i%3] = self.modify_lsb(channel, message_to_hide[i])
 
         for i in range(0, len(pixel_array)):
             pixel_array[i] = tuple(pixel_array[i])
 
-        print('mod : {0}'.format(pixel_array[j]))
-
         img.putdata(pixel_array)
         img.save('out/stego.bmp')
+        img.close()
 
     def extract(self, img_path):
         img = Image.open(img_path)
         pixel_array = self.get_pixel_array(img)
 
         extracted_bin = ''
-        for i in range(2, 58):
+        for i in range(0, self.get_capacity(img_path)):
             extracted_bin += bin(pixel_array[i//3][i%3])[-1:]
 
-        print('{0} and {1}'.format(extracted_bin, len(extracted_bin)))
-        extracted_hex = ''
-        for i in range(0, len(extracted_bin)//8):
-            print('hey : {0}'.format(extracted_bin[8*i:(8*i)+8]))
-            extracted_hex += hex(int(extracted_bin[8*i:8*i+8],2))[2:]
-        print(extracted_hex)
-        extracted = bytes.fromhex(extracted_hex)
-        return extracted.decode('utf-8')
+        # check signature
+        signature_length = len(self.signature_bin)
+        extracted_signature = extracted_bin[: signature_length]
+        if extracted_signature == self.signature_bin:
+            # check hiding method
+            extracted_hiding_method = extracted_bin[signature_length: signature_length + 2]
+            if extracted_hiding_method == '00':
+                # check is_string
+                extracted_is_string = extracted_bin[signature_length + 2: signature_length + 4]
+                if extracted_is_string == '01':
+                    length_bin = extracted_bin[signature_length + 4: signature_length + 36]
+                    length = int(length_bin, 2)
+                    string_bin = extracted_bin[signature_length + 36: signature_length + 36 + (length * 8)]
+
+                    extracted_string = binascii.unhexlify('%x' % (int(string_bin, 2)))
+
+                    return extracted_string.decode('utf-8')
+
+                elif extracted_is_string == '00':
+                    return NotImplementedError()
+                    # if no
+                        # get file_path length
+                        # get length
+                        # get file_path
+                        # get content
+                        # build the file
+                else:
+                    return False
+            else:
+                return NotImplementedError()
+        else:
+            return False
+    
+    def build_string_to_hide(self, is_string, input_data, hiding_method):
+        string_to_hide = ''
+        string_to_hide += self.signature_bin
+        string_to_hide += hiding_method
+        if is_string:
+            string_to_hide += '01'
+            string_data = input_data
+            #add length_in_bin 32b
+            string_to_hide += bin(len(string_data))[2:].zfill(32)
+            #add string
+            string_to_hide += self.string_to_bin(string_data)
+        else:
+            string_to_hide += '00'
+            file_path = input_data
+            with open(file_path, 'rb') as f:
+                file_byte = f.read()
+            f.close()
+            print('filebyte : {0}'.format(file_byte))
+            #add filename_length_in_bin 16b
+            string_to_hide += bin(len(file_path))[2:].zfill(16)
+            #add length_in_bin 32b
+            string_to_hide += bin(len(file_byte))[2:].zfill(32)
+            #add filename
+            string_to_hide += self.string_to_bin(basename(file_path))
+            #add content
+            content_hex = file_byte.hex()
+            string_to_hide += bin(int(content_hex, 16))[2:].zfill(8 * (len(content_hex)//2))
+        
+        return string_to_hide
+
+    def string_to_bin(self, input_data):
+        string_byte = input_data.encode("utf-8")
+        string_hex = string_byte.hex()
+        return bin(int(string_hex, 16))[2:].zfill(8 * (len(string_hex)//2))
 
     def get_pixel_array(self, img):
         width, height = img.width, img.height
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from libs.Stegano import SteganoBitmap
 
 
 def main():
-    stegano = SteganoBitmap('Key', False)
+    stegano = SteganoBitmap()
+    print(stegano.get_capacity('samples/jazzstops.bmp'))
     stegano.hide('samples/jazzstops.bmp', 'abcXYZ~')
 
     res = stegano.extract('out/stego.bmp')
